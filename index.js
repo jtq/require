99ac4d7154974f086c56622ef2bb2c5a4d11cea8
jtq <jamestpalmer1979@gmail.com>
2016-05-15 01:09:51 +0100

    Moved module processing/compilation into separate function

--- index.js
+++ index.js
@@ -15,27 +15,37 @@ var require = (function(rootUrl) {
 	  		if (req.readyState != 4 || req.status != 200) return;
 	  		var source = req.responseText;
 
-	  		var module = (function(source) {	// Create new scope for eval
 
-	  			var require = baseRequire.bind(null, thisModuleUrl);
 
-	  			var module = {
-	  				exports: {}
-	  			};
-	  			var exports = module.exports;
 
-	  			eval(source);
 
-	  			return module.exports;
-	  		})(source);
-
-	  		if(callback) {
-	  			callback(module);
-	  		}
+		  		process(thisModuleUrl, source, callback);
 		};
 		req.send();
 	};
 
+	var compile = function(source, thisModuleUrl) {	// Create new scope for eval
+
+		var require = baseRequire.bind(null, thisModuleUrl);
+		var module = {
+			exports: {}
+		};
+		var exports = module.exports;	/// Handle exports= and module.exports=
+
+		eval(source);
+
+		return module.exports;
+	};
+
+	var process = function(thisModuleUrl, source, callback) {
+		// Finally compile the source of our module now all the dependencies have been compiled
+		//console.log("compiling", thisModuleUrl);
+  		var module = compile(source, thisModuleUrl);
+  		//console.log("compiled", thisModuleUrl, module);
+
+  		if(callback) {
+  			callback(module);
+  		}
 	};
 
 	var resolvePath = function(baseUrl, relativePath) {
