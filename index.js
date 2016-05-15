75758f0841fc6e9af0a3b8751c9dacbc7c55eda6
jtq <jamestpalmer1979@gmail.com>
2016-05-15 01:24:40 +0100

    Added module cache and recursive submodule loading
    Still needs some work though - submodules abort redundant XHR requests
    for modules already cached by other requests as soon as they're
    detected, but there's no pre-emptive detection of shared dependencies to
    avoid issuing redundant requests in the first place.
    
    Also no checking for mutually-recursive dependency loops between modules
    yet.
    
    Probably be the same solution for both issues, though - some sort of
    pre-emptively registered "virtual" request (and empty "placeholder"
    module object) in the cache to indicate a dependency is on its way, that
    other modules can just register their own callbacks on instead of
    creating their own separate requests.

--- index.js
+++ index.js
@@ -2,24 +2,73 @@ var require = (function(globalRootUrl) {
 
 	globalRootUrl = globalRootUrl + "";
 
+	var cache = {};
+
 	var baseRequire = function(baseUrl, modulePath, onLoaded) {
 		// Convert relative module path to absolute and normalise to get rid of any ./ or ../
 		var thisModuleUrl = resolvePath(baseUrl, modulePath);
 
 		var callback = onLoaded;
 
-		// Get module source
+		// If we already have a cached copy of the module, synchronously return it (for modules that use `var mod = require('mod');`)
+		if(cache[thisModuleUrl]) {
+			if(callback) {	// Equally, if the calling code has passed in a callback to be run when this module is ready, run it as well
+	  			callback(cache[thisModuleUrl]);
+	  		}
+			return cache[thisModuleUrl];
+		}
+
+		// If not cached then we're loading/compiling asynchronously, so get module source from remote URL:
 		var req = new XMLHttpRequest();
 		req.open("GET", thisModuleUrl, true);
 		req.onreadystatechange = function () {
-	  		if (req.readyState != 4 || req.status != 200) return;
+
+			// If this module was already asynchronously loaded by another module requiring it before this request finished, dump out of this request
+			// (no sense in compiling the module twice) and call any callback on this request to say the module is already ready
+			if(cache[thisModuleUrl]) {
+				if(req.readyState != 4 && req.status != 0) { // We're aborting the request, so don't trigger the logic again when readystate changes one last time because of the abort
+					//console.log("cached", thisModuleUrl, "found (from XMLHttpRequest race condition) - skipping compilation");
+					req.abort();	// Don't care what stage this request is at - kill it because it's no longer needed
+					if(callback) {
+	  					callback(cache[thisModuleUrl]);
+	  				}
+	  			}
+				return;
+			}
+			if (req.readyState != 4 || req.status != 200) return;
+
 	  		var source = req.responseText;
 
+	  		// Check whether this module requires submodules, in which case compiling it will need deferring until we've downloaded/built those
+	  		var subModules = {}, subModuleUrls;
+	  		var matches;
+	  		var requireRegexp = /[\s=;{}]require\s*\(\s*['""]\s*([a-zA-Z0-9_\-\.\/\\]+)\s*['""]/g;
+	  		while((matches = requireRegexp.exec(source)) !== null) {
+	  			var subModuleUrl = resolvePath(thisModuleUrl, matches[1]);
+	  			subModules[subModuleUrl] = false;
+	  		}
+	  		subModuleUrls = Object.keys(subModules);	// Optimisation to avoid repeated calls to Object.keys(subModules)
+	  		//console.log("loading", thisModuleUrl, "- dependencies", subModuleUrls);
 
+	  		// No need to filter for already-cached modules here, as baseRequire() already handles that
 
-
-
+	  		if(subModuleUrls.length) {	// If this submodule has unmet dependencies, download/build them now
+		  		subModuleUrls.forEach(function(subModuleUrl) {
+		  			baseRequire("", subModuleUrl, function(module) {
+		  				// Now this subModule is compiled mark it off the "unmet dependencies" list
+		  				//console.log("submodule", subModuleUrl, "loaded for ", thisModuleUrl);
+		  				subModules[subModuleUrl] = true;
+		  				// And if it's the last one, finally compile the original module that depended on it
+		  				if(subModuleUrls.every(function(key) { return subModules[key]; })) {
+		  					//console.log("met dependencies for", thisModuleUrl, "- building");
+		  					process(thisModuleUrl, source, callback);
+		  				}
+		  			});
+		  		});
+		  	}
+		  	else {	// Otherwise (no unmet dependencies), build immediately
 		  		process(thisModuleUrl, source, callback);
+		  	}
 		};
 		req.send();
 	};
@@ -43,6 +92,8 @@ var require = (function(globalRootUrl) {
   		var module = compile(source, thisModuleUrl);
   		//console.log("compiled", thisModuleUrl, module);
 
+  		cache[thisModuleUrl] = module;
+
   		if(callback) {
   			callback(module);
   		}
@@ -59,5 +110,6 @@ var require = (function(globalRootUrl) {
 	};
 
 	var globalRequire = baseRequire.bind(null, globalRootUrl);
+	globalRequire.cache = cache;
 	return globalRequire;
 })(document.location);
